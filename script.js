56f133c4a300a17e2baba9a6759fb6fb1d3395b3
Kait OShaughnessy <oshaughnessykm@gmail.com>
2020-03-19 18:04:01 -0400

    beginning JavaScript - console.log start

---
+++ script.js
@@ -0,0 +1,13 @@
+//DOM queries
+var startButton = document.getElementById('startButton')
+
+//When start button is clicked...
+startButton.addEventListener('click', startQuiz)
+
+
+//Start quiz function
+function startQuiz() {
+    console.log("Quiz started")
+    
+}
+
